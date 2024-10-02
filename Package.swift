4ac19ed3bf8d268762946c3ca1ea7f0155ad12e2
tannerdsilva <t@tannersilva.com>
2024-10-02 17:53:19 -0500

    v1 release

--- Package.swift
+++ Package.swift
@@ -9,27 +9,37 @@ let package = Package(
     	.macOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Clibxlsxio",
             targets: ["Clibxlsxio"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
     	.target(
-            name: "Clibxlsxio",
-            dependencies: ["Cexpat", "minizip"]),
-       .target(
-			name: "minizip",
-			dependencies: []),
-        .target(
-        	name: "Cexpat",
-        	dependencies: []),
+            name:"Clibxlsxio",
+            dependencies:["Cexpat", "Clibzip"],
+            path:"./xlsxio",
+            exclude:[],
+            sources:["lib"],
+            publicHeadersPath:"include",
+            cSettings:[]
+        ),
+        .systemLibrary(
+			name: "Clibzip",
+			pkgConfig: "libzip",
+			providers: [
+				.brew(["libzip"]),
+				.apt(["libzip-dev"])
+			]
+		),
+        .systemLibrary(
+			name: "Cexpat",
+			pkgConfig: "expat",
+			providers: [
+				.brew(["expat"]),
+				.apt(["libexpat1-dev"])
+			]
+		)
     ],
     cxxLanguageStandard: .cxx20
 )
--- Sources/Clibxlsxio/include/Clibxlsxio.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDED_XLSXIO_H
-#define INCLUDED_XLSXIO_H
-
-#define USE_MINIZIP
-
-#include "xlsxio_read.h"
-#include "xlsxio_write.h"
-
-#endif
